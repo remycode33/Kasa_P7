b92cd1f580c84dde8d6ccd64dab47ff2558e71bf
remycode <remycode33@gmail.com>
2024-01-20 23:11:08 +0100

    update file extension in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Home from "./pages/home/Home.js";
-import About from "./pages/about/About.js";
-import Error404 from "./pages/error404/Error404.js";
+import Home from "./pages/home/Home.jsx";
+import About from "./pages/about/About.jsx";
+import Error404 from "./pages/error404/Error404.jsx";
 // import Logement from "./pages/logement/Logement.js";
 // import Error404 from "./pages/error404/Error404";
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import Logement from "./pages/logement/Logement.js";
+import Logement from "./pages/logement/Logement.jsx";
 
 function App() {
   return (
