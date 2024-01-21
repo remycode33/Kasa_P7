693d368be2e8414e72e72f8b23710fbbc0eb48e6
remycode <remycode33@gmail.com>
2024-01-21 12:06:08 +0100

    modify back link in home page

--- src/pages/error404/Error404.jsx
+++ src/pages/error404/Error404.jsx
@@ -1,22 +1,20 @@
-import './error404.scss'
+import "./error404.scss";
 import { Link } from "react-router-dom";
 import Header from "../../components/header/Header";
-import Footer from "../../components/footer/Footer"
+import Footer from "../../components/footer/Footer";
 
 function Error404() {
-
-    return(
-        <div className="errorPage">
-            <Header />
-            <h2>404</h2>
-            <h3>Oups! La page que vous demandez n'existe pas.</h3>
-            <Link  to='/'>
-                <div>Retourner sur la page d’accueil</div>
-            </Link> 
-            <Footer/>  
-        </div>
-    )
-
+  return (
+    <div className="errorPage">
+      <Header />
+      <h2>404</h2>
+      <h3>Oups! La page que vous demandez n'existe pas.</h3>
+      <Link to="/home">
+        <div>Retourner sur la page d’accueil</div>
+      </Link>
+      <Footer />
+    </div>
+  );
 }
 
-export default Error404
+export default Error404;
